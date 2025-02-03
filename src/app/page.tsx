80df820c2b3e40f7427d725b045b3c892088b13b
masud-rana44 <mdmasudrana4488@gmail.com>
2025-02-03 12:33:09 +0600

    Implementing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,61 @@
 import React from "react";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { ArrowRight, LogIn } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton afterSignOutUrl="/" />
+            </SignedIn>
+          </div>
 
-      <Button>Click Me</Button>
+          {/* If user is authenticated redirect to the first chat */}
+          <div className="flex mt-2">
+            {isAuth && (
+              <>
+                <Link href="/">
+                  <Button>
+                    Go to chats <ArrowRight className="ml-2" />
+                  </Button>
+                </Link>
+                <div className="ml-3">{/* Subscription Button */}</div>
+              </>
+            )}
+          </div>
+        </div>
+
+        <p className="max-w-xl mt-1 text-lg text-slate-600">
+          Join millions of students, researchers and professionals to instatly
+          answer questions and unserstand research with AI
+        </p>
+
+        <div className="w-full mt-4">
+          {isAuth ? (
+            <></>
+          ) : (
+            <Link href="/sign-in">
+              <Button>
+                Login to get started!
+                <LogIn className="w-4 h-4 ml-2" />
+              </Button>
+            </Link>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
